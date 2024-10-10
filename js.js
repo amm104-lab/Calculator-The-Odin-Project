06106d5d76adef2040b2516afa96b0ee5a0cb98e
amm <amm@tutao.de>
2024-10-10 09:12:46 +0200

    Created html,css and js files.

---
+++ js.js
@@ -0,0 +1,88 @@
+//simple math arrow functions
+let add = (a, b) => a+b;
+let sub = (a, b) => a-b;
+let divi = (a, b) => a/b ;
+let mult = (a, b) => a*b;
+
+//choosing math function based on operator
+let operate = (op,a,b) => {
+    const mathFunction = getMathFunction(op)
+    return mathFunction(a, b)
+}
+    
+                /**
+                 * Returns mathematical function dependend on the input parameter                 
+                 */
+function getMathFunction(op)  {
+    switch (op) {
+        case "+":
+            return add            
+            case "-":
+            return sub            
+            case "x":
+            return mult            
+            case "/":
+            return divi            
+        default:
+            throw new Error("unknown function")
+    }
+
+}
+
+
+//declaration of constants for events
+const display = document.querySelector(".window");
+const buttons = document.getElementsByClassName("numbers");
+const ops = document.getElementsByClassName("operator");
+const equ = document.getElementById("=");
+const clear = document.getElementById("clear");
+
+//on click events for "=" and "clear"
+equ.onclick = () => {
+    display.textContent = solve();//showing the result of the solve function(l.81) on the display
+}
+clear.onclick = () => {
+    display.textContent = "";//emptying the display
+}
+
+//for loop to bring the numbers on the screen
+for (let i = 0 ; i < buttons.length ; i++){
+    (function(index){
+        buttons[index].onclick = function(){
+        dis = index;
+        display.textContent += dis;
+        };
+    })(i)}
+
+//for loop to bring the operators on the screen assigning them an index
+for (let j = 0 ; j < ops.length ; j++){
+    (function(index){
+        ops[index].onclick = function(){
+        dos = index;
+            if(index === 0){
+                dos = "+";
+            }
+            else if(index === 1){
+                dos="-";
+            }
+            else if(index === 2){
+                dos="x";
+            }
+            else{
+                dos = "/";
+            }
+        display.textContent += " " + dos + " ";
+        };
+    })(j)}
+
+ function help(){
+    arr = document.getElementsByClassName("window")[0].innerText.split(" "); //seperating
+    oper = arr[1]; //assigning a variable the operator
+    arr.splice(0,3, arr[0], arr[2]); //taking out the operator with index[1]
+    return arr;}
+
+function solve(){
+    let c = Number(help()[0]); //turning the seperated numbers from the help function(l. 74) into numbers and asigning them to a variable
+    let d = Number(help()[1]);
+    let sol = operate(oper,c,d); //giving the numbers and the operator to the operate function(l.8) and assigning it a variable
+    return sol;}
