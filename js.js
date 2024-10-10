63cb394eda2f69fdb40a8e9cd44ff2ad7fe53867
amm <amm@tutao.de>
2024-10-10 16:25:11 +0200

    review with bed

--- js.js
+++ js.js
@@ -3,8 +3,7 @@ let add = (a, b) => a+b;
 let sub = (a, b) => a-b;
 let divi = (a, b) => a/b ;
 let mult = (a, b) => a*b;
-let operations = 0;
-
+let globalDisplayContent = ""
 
 //choosing math function based on operator
 let operate = (op,a,b) => {
@@ -30,84 +29,117 @@ function getMathFunction(op)  {
 
 }
 
-function seeIfNext(){
-    solve();
-    if(next==undefined){
-        display.textContent = solve();
-    }
-    else{
-        display.textContent = solve() + " " + next + " ";
-        operations++
-    }
-}
 
 //declaration of constants for events
 const display = document.querySelector(".window");
-const buttons = document.getElementsByClassName("numbers");
+const numberButtons = document.getElementsByClassName("numbers");
 const ops = document.getElementsByClassName("operator");
 const equ = document.getElementById("=");
-const clear = document.getElementById("clear");
-const operat = document.querySelector(".operate")
+const clearButton = document.getElementById("clear");
 
-operat.addEventListener("click", () => {
-   
-    if(operations>=1){
-        seeIfNext();
-        }
-    else{
-        operations++;
-    }
-})
+
 
 //on click events for "=" and "clear"
 equ.onclick = () => {
-    seeIfNext();//showing the result of the solve function(l.81) on the display
+    const result = solve()
+    if( result) {
+      setDisplayResult(result)
+    } 
 }
-clear.onclick = () => {
-    display.textContent = "";//emptying the display
-    operations = 0;
+
+clearButton.onclick = function () {
+    clearDisplayContent()
 }
+
+
+function setDisplayResult(result) {
+    globalDisplayContent = String(result)
+    updateDisplay()        
+}
+
+function clearDisplayContent() {
+    globalDisplayContent = ""
+    updateDisplay()        
+}
+
+function addNumber(additionContent) {
+    globalDisplayContent += additionContent   
+    updateDisplay()
+}
+
+
+function addOperator(operator) {
+    if( containsOperator()) {
+        // we already have an operator so we want to solve the statement first and take the result as first operand
+        const result = solve()
+        if(result) {
+            setDisplayResult(result)            
+            addOperator(operator)
+        } else {
+            // no result
+        }
+    } else {        
+        globalDisplayContent += " " + operator  + " "
+        updateDisplay()
+    }
+    
+}
+
+
+function containsOperator() {
+    if( getContentParts(globalDisplayContent)[1] ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function updateDisplay() {
+// update html element
+    display.textContent = globalDisplayContent;
+}
+
+
 
 //for loop to bring the numbers on the screen
-for (let i = 0 ; i < buttons.length ; i++){
-    (function(index){
-        buttons[index].onclick = function(){
-        dis = index;
-        display.textContent += dis;
-        };
-    })(i)}
+for (let i = 0 ; i < numberButtons.length ; i++){
+   numberButtons[i].onclick =  () => {    
+       addNumber(i)
+   };
+}    
 
 //for loop to bring the operators on the screen assigning them an index
-for (let j = 0 ; j < ops.length ; j++){
-    (function(index){
-        ops[index].onclick = function(){
-        dos = index;
+for( let index = 0 ; index < ops.length ; index++) {
+        ops[index].onclick = () => {
             if(index === 0){
-                dos = "+";
+                addOperator("+");
             }
             else if(index === 1){
-                dos="-";
+                addOperator("-");
             }
             else if(index === 2){
-                dos="x";
+                addOperator("x");
             }
             else{
-                dos = "/";
-            }
-        display.textContent += " " + dos + " ";
+                addOperator("/");
+            }        
         };
-    })(j)}
+}
 
- function help(){
-    arr = document.getElementsByClassName("window")[0].innerText.split(" "); //seperating
-    oper = arr[1]; //assigning a variable the operator
-    next = arr[3]    
-    arr.splice(0,3, arr[0], arr[2]); //taking out the operator with index[1]
-    return arr;}
+
+function getContentParts(displayContent) {
+    return displayContent.split(" "); //seperating
+}
+
 
 function solve(){
-    let c = Number(help()[0]); //turning the seperated numbers from the help function(l. 74) into numbers and asigning them to a variable
-    let d = Number(help()[1]);
-    let sol = operate(oper,c,d); //giving the numbers and the operator to the operate function(l.8) and assigning it a variable
-    operations = 0;
-    return sol;}
+    const parts = getContentParts(globalDisplayContent)
+    if( parts.length === 3 ) {
+        const firstOperand = Number(parts[0]); //turning the seperated numbers from the help function(l. 74) into numbers and asigning them to a variable
+        const secondOperand = Number(parts[2]);
+        const oper = parts[1]
+        return operate(oper,firstOperand,secondOperand); //giving the numbers and the operator to the operate function(l.8) and assigning it a variable
+    }
+    return null
+    
+}
